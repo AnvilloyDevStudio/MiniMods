cfe601a2f14908f126628c1663ff5f6b4dae65fc
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2022-12-17 01:24:29 +0800

    Added Core-Redstone

--- templates/minimods-mods-development-offi/src/main/java/io/github/anvilloystudio/minimods/mod/core/Main.java
+++ templates/minimods-mods-development-offi/src/main/java/io/github/anvilloystudio/minimods/mod/core/Main.java
@@ -1,44 +1,4 @@
-package minimods.mod.core;
-
-import java.io.IOException;
-import java.util.List;
-
-import javax.imageio.ImageIO;
-
-import org.spongepowered.asm.mixin.injection.Inject;
-
-import io.gfx.GraphicComp;
-import io.gfx.GraphicComp.ModSprite;
-import io.github.anvilloystudio.minimods.core.Mods;
-import minicraft.core.Game;
-import minicraft.core.io.Sound;
-import minicraft.entity.Direction;
-import minicraft.entity.Entity;
-import minicraft.entity.mob.Mob;
-import minicraft.entity.mob.Player;
-import minicraft.entity.particle.SmashParticle;
-import minicraft.entity.particle.TextParticle;
-import minicraft.gfx.Color;
-import minicraft.gfx.Screen;
-import minicraft.gfx.Sprite;
-import minicraft.gfx.SpriteSheet;
-import minicraft.item.Item;
-import minicraft.item.Items;
-import minicraft.item.Recipe;
-import minicraft.item.Recipes;
-import minicraft.item.StackableItem;
-import minicraft.item.TileItem;
-import minicraft.item.ToolItem;
-import minicraft.level.Level;
-import minicraft.level.LevelGen;
-import minicraft.level.tile.OreTile;
-import minicraft.level.tile.Tile;
-import minicraft.level.tile.Tiles;
-import minicraft.level.tile.TreeTile;
-import minicraft.level.tile.OreTile.OreType;
-import minicraft.screen.Display;
-import minimods.mod.core.mixins.PausedDisplayMixin;
-import minimods.mod.core.mixins.WorldMixin;
+package io.github.anvilloystudio.minimods.mod.core;
 
 public class Main {
 	public static void main(String[] args) {
