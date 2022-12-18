0d68feadae66d12f4eefea53b9a3290e582a00a3
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2022-12-19 00:43:47 +0800

    Added SpriteMixin

--- full-build.bat
+++ full-build.bat
@@ -1,3 +1,6 @@
-call "gradlew.bat" build
+call "gradlew.bat" :build
+call "gradlew.bat" :mod-core-ores:build
+call "gradlew.bat" :mod-core-redstone:build
+call "gradlew.bat" :mod-debug-test:build
 call "gradlew.bat" :minicraft-base:build
 call "final-build.bat" minimods-2.0.7-0.4.0.jar minicraft_plus-2.0.7.jar
