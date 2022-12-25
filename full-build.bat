03c2bec56732f2eeb8bd992e7c20484a1e715f53
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2022-12-25 21:14:44 +0800

    Moved wrench to extra

--- full-build.bat
+++ full-build.bat
@@ -1,6 +1,7 @@
 call "gradlew.bat" :build
 call "gradlew.bat" :mod-core-ores:build
 call "gradlew.bat" :mod-core-redstone:build
+call "gradlew.bat" :mod-core-redstone-extra:build
 call "gradlew.bat" :mod-debug-test:build
 call "gradlew.bat" :minicraft-base:build
 call "final-build.bat" minimods-2.0.7-0.4.0.jar minicraft_plus-2.0.7.jar
